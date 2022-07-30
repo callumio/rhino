6fe8df31386e6728ee9827f0962baca6553f5fb7
Callum Leslie <git@cleslie.uk>
2022-07-31 00:45:12 +0100

    feat: basic logic for running program

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,10 @@ use args::RhinoArgs;
 fn main() {
     let args: RhinoArgs = argh::from_env();
     let program_name: String = args.name.unwrap_or(args.cmd);
+    if !(args.invert ^ search(&program_name)) {
+        todo!("run the program");
+    }
+    else {
+        todo!("dont run program");
+    }
 }
