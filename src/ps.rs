e9d8ba46edae8d890a0df2ca88f6bea8b05127b1
Callum Leslie <git@cleslie.uk>
2022-07-31 00:42:59 +0100

    feat: process logic for search complete

--- src/ps.rs
+++ src/ps.rs
@@ -1,9 +1,12 @@
 use std::process::Command;
+use std::str;
 
-pub fn run(){
-
+pub fn run(cmd: &str, args: &str) {
+    let args_vec: Vec<&str> = args.split(' ').collect();
+    let run_cmd = Command::new(cmd).args(args_vec).spawn().expect("Error starting command");
 }
 
-pub fn search(){
-
+pub fn search(program: &str) -> bool {
+    let search_cmd = Command::new("pgrep").args(["-ix", program]).output().unwrap();
+    !matches!(search_cmd.stdout.as_slice(), b"")
 }
