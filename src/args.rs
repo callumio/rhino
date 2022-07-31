f5a35b9bc608f18edc409777cf0e138f9f05121e
Callum Leslie <git@cleslie.uk>
2022-07-31 17:03:59 +0100

    feat: add currently needed arg flags

--- src/args.rs
+++ src/args.rs
@@ -3,13 +3,27 @@ use argh::FromArgs;
 #[derive(Debug, FromArgs)]
 /// todo
 pub struct RhinoArgs {
-    /// which program to check if it is running
-    #[argh(option, short = 'n')]
-    pub name: Option<String>,
-
+ 
     /// invert the program - run if already running
     #[argh(switch, short = 'i')]
     pub invert: bool,
+
+    /// search for the process, case sensitively - by default it uses smart case
+    #[argh(switch, short = 'c')]
+    pub case_sensitive: bool,
+
+    /// allow partial matches in the process search - by default it only allows exact matches 
+    #[argh(switch, short = 'p')]
+    pub partial: bool,
+
+    /// require all processes to match, to continue - by default at least one must be present 
+    #[argh(switch, short = 'a')]
+    pub all: bool,
+
+    /// which program to check if it is running, delimited by a space - by default the command to
+    /// be ran will be checked
+    #[argh(option, short = 'n')]
+    pub name: Option<String>,
 
     /// the command to be ran
     #[argh(positional)]
